038e93cdc43859f4422c6a790e657bf1a574b699
Phil <pjguitar15@gmail.com>
2022-05-19 12:46:18 +0800

    improved join us styling

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -25,9 +25,8 @@ const ContactUs = () => {
       <Container data-aos='flip-left' data-aos-duration='1000' className='py-5'>
         <h4>Get in Touch</h4>
         <p className='text-muted col-lg-5'>
-          If you have any questions or you just want to know more about ILTP,
-          please fill out the form below. We do our best to respond as soon as
-          we can.
+          If you have any questions about ILTP, please fill out the form below.
+          We do our best to respond as soon as we can.
         </p>
         <ContactForm />
       </Container>
--- src/pages/HomeSections/JoinUs.jsx
+++ src/pages/HomeSections/JoinUs.jsx
@@ -11,29 +11,23 @@ const JoinUs = () => {
   return (
     // background image is in join-us class
     <div className='join-us text-center'>
-      <Container>
-        <div className='col-10 mx-auto become-a-member-div p-4 rounded-2 mb-4 bg-white'>
-          <h1 className='join-us-h1 bebas text-warning display-1 m-0'>
-            Join Us!
-          </h1>
-          <h5 className='text-muted'>Become a member/Support Us!</h5>
-          <p className='lead font-italic col-10 mx-auto'>
-            International Leadering Training Program is a non-profit
-            organization. Our goal is to be able to host and coordinate events
-            and activities youth and students can participate in for little to
-            no cost.
-          </p>
-        </div>
-        <Button
-          onClick={() => navigate('/contact')}
-          className='btn-warning mx-1'
-        >
-          Join Us
-        </Button>
-        <Button className='btn-warning mx-1'>
-          <i className='fa fa-paypal me-1'></i>Donate
-        </Button>
-      </Container>
+      <div className='col-12 mx-auto become-a-member-div p-4 mb-4 bg-white'>
+        <h1 className='join-us-h1 bebas text-warning display-1 m-0'>
+          Join Us!
+        </h1>
+        <h5 className='text-muted'>Become a member/Support Us!</h5>
+        <p className='lead font-italic col-lg-7 mx-auto'>
+          International Leadering Training Program is a non-profit organization.
+          Our goal is to be able to host and coordinate events and activities
+          youth and students can participate in for little to no cost.
+        </p>
+      </div>
+      <Button onClick={() => navigate('/contact')} className='btn-warning mx-1'>
+        Join Us
+      </Button>
+      <Button className='btn-warning mx-1'>
+        <i className='fa fa-paypal me-1'></i>Donate
+      </Button>
     </div>
   )
 }
